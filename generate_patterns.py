de5b9da2d3e816d678fcbe41b30eb9d184583660
Miguel Aguilera <miguel@maguilera.net>
2016-04-12 16:52:18 +0200

    Update generate_patterns.py

--- generate_patterns.py
+++ generate_patterns.py
@@ -14,20 +14,10 @@ def generate_sinusoidal(n,L,T):
 	data=np.array([],int)
 	for i in range(n):
 		x0=(np.sin(np.arange(L)*2*np.pi/T)+1)*0.5
-		x=(np.random.rand(L)>(1*x0+0.0)).astype(int)
+		x=(np.random.rand(L)>(x0)).astype(int)
 		if len(data):
 			data=np.vstack((data,x))
 		else:
 			data=x
 	return data
-	
-if __name__ == '__main__':
-	n=100
-	TA=3
-	TB=7
-	L=30
-	dataA=generate_sinusoidal(n,L,TA)
-	dataB=generate_sinusoidal(n,L,TB)
-	dataC=generate_random(n,L)
-	data=np.vstack((dataA,dataB,dataC))
-	np.save('movement-patterns',data)
+		
